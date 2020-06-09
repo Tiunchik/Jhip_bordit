e23e27d2734fc97baae6b0e5072f56e7ec03b3e3
Tiunchik <Senebh@gmail.com>
2020-06-09 18:39:45 +0300

    add point to orders-details.vue

--- src/main/webapp/app/entities/orders/orders-details.component.ts
+++ src/main/webapp/app/entities/orders/orders-details.component.ts
@@ -2,10 +2,20 @@ import { Component, Vue, Inject } from 'vue-property-decorator';
 
 import { IOrders } from '@/shared/model/orders.model';
 import OrdersService from './orders.service';
+import OrderPointService from '@/entities/order-point/order-point.service';
+import { IOrderPoint } from '@/shared/model/order-point.model';
+import { mixins } from 'vue-class-component';
+import AlertMixin from '@/shared/alert/alert.mixin';
 
 @Component
-export default class OrdersDetails extends Vue {
+export default class OrdersDetails extends mixins(AlertMixin) {
   @Inject('ordersService') private ordersService: () => OrdersService;
+  @Inject('orderPointService') private orderPointService: () => OrderPointService;
+  private removeId: number = null;
+
+  public orderPoints: IOrderPoint[] = [];
+
+  public isFetching = false;
   public orders: IOrders = {};
 
   beforeRouteEnter(to, from, next) {
@@ -26,5 +36,53 @@ export default class OrdersDetails extends Vue {
 
   public previousState() {
     this.$router.go(-1);
+  }
+
+  public mounted(): void {
+    this.retrieveAllOrderPoints();
+  }
+
+  public clear(): void {
+    this.retrieveAllOrderPoints();
+  }
+
+  public retrieveAllOrderPoints(): void {
+    this.isFetching = true;
+
+    this.orderPointService()
+      .retrieve()
+      .then(
+        res => {
+          this.orderPoints = res.data;
+          this.isFetching = false;
+        },
+        err => {
+          this.isFetching = false;
+        }
+      );
+  }
+
+  public prepareRemove(instance: IOrderPoint): void {
+    this.removeId = instance.id;
+    if (<any>this.$refs.removeEntity) {
+      (<any>this.$refs.removeEntity).show();
+    }
+  }
+
+  public removeOrderPoint(): void {
+    this.orderPointService()
+      .delete(this.removeId)
+      .then(() => {
+        const message = 'A OrderPoint is deleted with identifier ' + this.removeId;
+        this.alertService().showAlert(message, 'danger');
+        this.getAlertFromStore();
+        this.removeId = null;
+        this.retrieveAllOrderPoints();
+        this.closeDialog();
+      });
+  }
+
+  public closeDialog(): void {
+    (<any>this.$refs.removeEntity).hide();
   }
 }
--- src/main/webapp/app/entities/orders/orders-details.vue
+++ src/main/webapp/app/entities/orders/orders-details.vue
@@ -27,8 +27,85 @@
                 <router-link v-if="orders.id" :to="{name: 'OrdersEdit', params: {ordersId: orders.id}}" tag="button" class="btn btn-primary">
                     <font-awesome-icon icon="pencil-alt"></font-awesome-icon>&nbsp;<span> Edit</span>
                 </router-link>
+                <router-link :to="{name: 'OrderPointCreate'}" tag="button" id="jh-create-entity" class="btn btn-primary float-right jh-create-entity create-order-point">
+                    <font-awesome-icon icon="plus"></font-awesome-icon>
+                    <span >
+                    Create a new Order Point
+                </span>
+                </router-link>
+                <b-alert :show="dismissCountDown"
+                         dismissible
+                         :variant="alertType"
+                         @dismissed="dismissCountDown=0"
+                         @dismiss-count-down="countDownChanged">
+                    {{alertMessage}}
+                </b-alert>
             </div>
         </div>
+        <div class="alert alert-warning" v-if="!isFetching && orderPoints && orderPoints.length === 0">
+            <span>No orderPoints found</span>
+        </div>
+        <div class="table-responsive" v-if="orderPoints && orderPoints.length > 0">
+            <table class="table table-striped">
+                <thead>
+                <tr>
+                    <th><span>Point ID</span></th>
+                    <th><span>Item name</span></th>
+                    <th><span>quantity</span></th>
+                    <th><span>Order number</span></th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr v-for="orderPoint in orderPoints" v-if="orderPoint.orders && orderPoint.orders.id === orders.id"
+                    :key="orderPoint.id">
+                    <td>
+                        <router-link :to="{name: 'OrderPointView', params: {orderPointId: orderPoint.id}}">{{orderPoint.id}}</router-link>
+                    </td>
+                    <td>
+                        <div v-if="orderPoint.goods">
+                            <router-link :to="{name: 'GoodsView', params: {goodsId: orderPoint.goods.id}}">{{orderPoint.goods.name}}</router-link>
+                        </div>
+                    </td>
+                    <td>{{orderPoint.number}}</td>
+                    <td>
+                        <div v-if="orderPoint.orders">
+                            <router-link :to="{name: 'OrdersView', params: {ordersId: orderPoint.orders.id}}">{{orderPoint.orders.id}}</router-link>
+                        </div>
+                    </td>
+                    <td class="text-right">
+                        <div class="btn-group">
+                            <router-link :to="{name: 'OrderPointView', params: {orderPointId: orderPoint.id}}" tag="button" class="btn btn-info btn-sm details">
+                                <font-awesome-icon icon="eye"></font-awesome-icon>
+                                <span class="d-none d-md-inline">View</span>
+                            </router-link>
+                            <router-link :to="{name: 'OrderPointEdit', params: {orderPointId: orderPoint.id}}"  tag="button" class="btn btn-primary btn-sm edit">
+                                <font-awesome-icon icon="pencil-alt"></font-awesome-icon>
+                                <span class="d-none d-md-inline">Edit</span>
+                            </router-link>
+                            <b-button v-on:click="prepareRemove(orderPoint)"
+                                      variant="danger"
+                                      class="btn btn-sm"
+                                      v-b-modal.removeEntity>
+                                <font-awesome-icon icon="times"></font-awesome-icon>
+                                <span class="d-none d-md-inline">Delete</span>
+                            </b-button>
+                        </div>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+        <b-modal ref="removeEntity" id="removeEntity" >
+            <span slot="modal-title"><span id="borditApp.orderPoint.delete.question">Confirm delete operation</span></span>
+            <div class="modal-body">
+                <p id="jhi-delete-orderPoint-heading">Are you sure you want to delete this Order Point?</p>
+            </div>
+            <div slot="modal-footer">
+                <button type="button" class="btn btn-secondary" v-on:click="closeDialog()">Cancel</button>
+                <button type="button" class="btn btn-primary" id="jhi-confirm-delete-orderPoint" v-on:click="removeOrderPoint()">Delete</button>
+            </div>
+        </b-modal>
     </div>
 </template>
 
